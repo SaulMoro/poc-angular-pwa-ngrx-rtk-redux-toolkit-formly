3d792d927be14f0c95f533b77a51baf91efeb0c5
Saul <smoro@atsistemas.com>
2020-10-26 13:09:11 +0100

    fix: remove commitizen on husky

--- husky.config.js
+++ husky.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   hooks: {
     'pre-commit': 'pretty-quick --staged && lint-staged -c lint-staged.config.js',
-    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
