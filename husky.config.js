247729507f4f1639380bb327b7c0028bb4ac64a3
Saul <smoro@atsistemas.com>
2020-11-03 17:51:16 +0100

    chore: simplify husky and lintstaged

--- husky.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  hooks: {
-    'pre-commit': 'pretty-quick --staged && lint-staged -c lint-staged.config.js',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-  },
-};
--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'src/app/**/*.ts': ['sh ./tools/lint.sh 1'],
-  'src/app/**/*.{scss,css}': ['sh ./tools/lint.sh 2'],
-  //'src/app/**/*.{js,json,md,html}': ['sh ./tools/lint.sh 0'],
-};
