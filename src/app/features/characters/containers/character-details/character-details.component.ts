ca30e9d477d44d7597745fbb69bbe90f8a59a93a
Saul <smoro@atsistemas.com>
2020-11-27 11:38:15 +0100

    refactor: first seo refactor

--- src/app/features/characters/containers/character-details/character-details.component.ts
+++ src/app/features/characters/containers/character-details/character-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { CharactersSelectors } from '@app/shared/data-access-characters';
 import { Character, Episode } from '@app/shared/models';
@@ -16,8 +18,18 @@ export class CharacterDetailsComponent implements OnInit {
   character$: Observable<Character> = this.store.select(CharactersSelectors.getSelectedCharacter);
   loading$: Observable<boolean> = this.store.select(CharactersSelectors.getLoadingCharacter);
   episodes$: Observable<Episode[]> = this.store.select(CharactersSelectors.getEpisodesOfSelectedCharacter);
+  seoConfig$ = this.character$.pipe(
+    switchMap(({ name }) =>
+      this.translocoService.selectTranslateObject('CHARACTERS.SEO_DETAILS', {
+        title: { name },
+        description: { name },
+        'keywords.0': { name },
+        'keywords.1': { name },
+      })
+    )
+  );
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store, private translocoService: TranslocoService) {}
 
   ngOnInit(): void {}
 
