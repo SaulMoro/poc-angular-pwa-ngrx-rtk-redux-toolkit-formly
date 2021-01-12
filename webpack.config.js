8a2b6741c794ef73fae244ddfa37f375038833c5
Saul <smoro@atsistemas.com>
2021-01-12 11:50:42 +0100

    feat: new tailwind version

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,8 @@
-const { merge } = require('webpack-merge');
+const { patchPostCSS } = require('@ngneat/tailwind');
 
 module.exports = (config) => {
   const isProd = config.mode === 'production';
   const tailwindConfig = require('./tailwind.config.js')(isProd);
-
-  return merge(config, {
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              ident: 'postcss',
-              syntax: 'postcss-scss',
-              plugins: [require('postcss-import'), require('tailwindcss')(tailwindConfig), require('autoprefixer')],
-            },
-          },
-        },
-      ],
-    },
-  });
+  patchPostCSS(config, tailwindConfig);
+  return config;
 };
